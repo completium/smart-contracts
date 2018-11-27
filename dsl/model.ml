ff7badfce4458fd2635d55e3d9bd4c89573dfc59
Benoit Rognier <benoit.rognier@edukera.com>
2018-11-27 10:40:15 +0100

    up

--- dsl/model.ml
+++ dsl/model.ml
@@ -8,6 +8,7 @@ type basic_type =
   | String
   | Double
   | Date
+  | Address
   | Tez
 
 type ident = string
@@ -15,6 +16,7 @@ type ident = string
 (* struct ****************************************************************************)
 
 type field_type =
+  | Identifier of basic_type
   | Basic of basic_type
   | Ref   of ident
 
@@ -60,13 +62,15 @@ let dump_type = function
   | Uint   -> "uint"
   | String -> "string"
   | Double -> "double"
+  | Address -> "address"
   | Date   -> "date"
   | Tez    -> "tez"
 
 let dump_field (id,ft) =
   match ft with
-  | Basic t -> id ^ " : " ^ (dump_type t)
-  | Ref   t -> id ^ " : " ^ t ^ " ref"
+  | Identifier t -> id ^ " : " ^ (dump_type t)^" (identifier)"
+  | Basic      t -> id ^ " : " ^ (dump_type t)
+  | Ref        t -> id ^ " : " ^ t ^ " ref"
 
 let dump_fields fs = String.concat "\n\t" (List.map dump_field fs)
 
@@ -108,6 +112,8 @@ let mk_cons id typ = fun m -> m @ [Const (id, typ)]
 
 let mk_asset id fds = fun m -> m @ [Asset (id, fds)]
 
+let mk_identifier id typ = (id, Identifier typ)
+
 let mk_field id typ = (id, Basic typ)
 
 let mk_field_ref id typ = (id, Ref typ)
@@ -132,69 +138,23 @@ let mk_transition smid trid tr =
   test
  ***********************************************************************************)
 
-(* built-in *)
-type uint = int
-
-type account
-
-(* smart contract specification *)
-module type [@smartcontract] Escrow = sig
-
-  val symbol : string
-
-  val name   : string
-
-  val total  : uint
-
-  type [@asset] tokenHolder = {
-      holder  : account ref;
-      balance : uint;
-    }
-
-  type [@transaction] transfer = {
-      amount   : uint;
-      toHolder : account ref;
-    }
-
-  type [@sm] states =
-    | Created    [@initial]
-    | Aborted    [@terminal]
-    | Confirmed
-    | Failed     [@terminal]
-    | Transfered [@terminal]
-
-  (* How to pass properties to transaction ? *)
-  type [@transition
-        fromState = Created;
-        toState   = Aborted;
-        roles     = ANY;
-       ] abort
-
-end
-
-(**
-   constant symbol of string
-
-   constant name of string
-
-   constant total of uint
-
-   asset tokenHolder {
-      holder  : account ref;
-      balance : uint;
-   }
- *)
 let _ =
   (* TODO : implement extension let%model m = ... as let m = empty >> ... *)
-  let m =
+  print_endline "\nERC20\n";
+  let erc20_model =
     empty >>
       mk_cons "symbol" String >>
       mk_cons "name"   String >>
       mk_cons "total"  Uint >>
       mk_asset "tokenHolder" [
-          mk_field_ref "holder" "account";
-          mk_field     "balance" Uint
-        ] >>
+          mk_identifier   "holder"  Address;
+          mk_field        "balance" Uint
+        ]
+  in
+  print_endline (dump_model erc20_model);
+  print_endline "\nEscrow\n";
+  let escrow_model =
+    empty >>
       mk_state_machine "sm" [
           "Created",    SInitial;
           "Aborted",    STerminal;
@@ -207,4 +167,42 @@ let _ =
           mFromState = "Created";
           mToState   = "Aborted";
         } in
-  print_endline (dump_model m)
+  print_endline (dump_model escrow_model)
+
+(*******************************************************************************
+  Transactions (aka logic)
+ *******************************************************************************)
+
+(* basic real types for transaction logic *)
+type uint    = int
+type double
+type date
+type address = FakeAddress
+type tez
+
+type sender = {
+    address : address;
+    balance : uint;
+  }
+
+type assets
+
+(* dynamically generated structs *)
+
+type tokenHolder = {
+    holder : address;
+    balance : uint;
+  }
+
+let getTokenHolder : assets -> address -> tokenHolder =
+  fun _ _ -> { holder = FakeAddress; balance = 0; }
+
+let setTokenHolder : assets -> tokenHolder -> assets = fun a _ -> a
+
+(* actual transation code ****************************************************)
+
+let transfer iAssets iSender iQty =
+  let lTokenholder = getTokenHolder iAssets iSender.address in
+  let lQty = lTokenholder.balance in
+  let lTokenholder = { lTokenholder with balance = lQty + iQty } in
+  setTokenHolder iAssets lTokenholder
