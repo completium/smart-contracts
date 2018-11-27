d26c5973f41eb7d9ad0b92d73c0bd1cc1167b367
Benoit Rognier <benoit.rognier@edukera.com>
2018-11-27 11:20:46 +0100

    up

--- dsl/model.ml
+++ dsl/model.ml
@@ -16,11 +16,17 @@ type ident = string
 (* struct ****************************************************************************)
 
 type field_type =
-  | Identifier of basic_type
-  | Basic of basic_type
-  | Ref   of ident
+  | FIdentifier of basic_type
+  | FBasic      of basic_type
+  | FRef        of ident
 
 type field = ident * field_type
+
+type arg_type =
+  | ABasic of basic_type
+  | ARef   of ident
+
+type arg   = ident * arg_type
 
 (* state machine ********************************************************************)
 
@@ -40,7 +46,7 @@ let empty_transition = { mId = ""; mFromState = ""; mToState = ""; }
 type state_machine = {
     mStates      : state list;
     mTransitions : transition list;
-    mArgs        : field list;
+    mArgs        : arg list;
   }
 
 let empty_machine = { mStates = []; mTransitions = []; mArgs = []; }
@@ -48,9 +54,10 @@ let empty_machine = { mStates = []; mTransitions = []; mArgs = []; }
 (* entity and model ******************************************************************)
 
 type entity =
-  | Const    of ident * basic_type
-  | Asset    of ident * field list
-  | Machine  of ident * state_machine
+  | Const       of ident * basic_type
+  | Asset       of ident * field list
+  | Machine     of ident * state_machine
+  | Transaction of ident * arg list
 
 type model = entity list
 
@@ -68,11 +75,18 @@ let dump_type = function
 
 let dump_field (id,ft) =
   match ft with
-  | Identifier t -> id ^ " : " ^ (dump_type t)^" (identifier)"
-  | Basic      t -> id ^ " : " ^ (dump_type t)
-  | Ref        t -> id ^ " : " ^ t ^ " ref"
+  | FIdentifier t -> id ^ " : " ^ (dump_type t)^" (identifier)"
+  | FBasic      t -> id ^ " : " ^ (dump_type t)
+  | FRef        t -> id ^ " : " ^ t ^ " ref"
 
 let dump_fields fs = String.concat "\n\t" (List.map dump_field fs)
+
+let dump_arg (id,arg) =
+  match arg with
+  | ABasic      t -> id ^ " : " ^ (dump_type t)
+  | ARef        t -> id ^ " : " ^ t ^ " ref"
+
+let dump_args fs = String.concat "\n\t" (List.map dump_arg fs)
 
 let dump_statety = function
   | SBasic -> ""
@@ -96,6 +110,7 @@ let dump_entity = function
   | Const (id,t)  -> "constant "^id^" of "^(dump_type t)
   | Asset (id,fs) -> "asset "^id^" {\n\t"^(dump_fields fs)^"\n}"
   | Machine (id,m) -> "machine "^id^" {\n\t"^(dump_machine m)^"\n}"
+  | Transaction (id,fs) -> "transaction "^id^" {\n\t"^(dump_args fs)^"\n}"
 
 let dump_model m = String.concat "\n\n" (List.map dump_entity m)
 
@@ -112,11 +127,15 @@ let mk_cons id typ = fun m -> m @ [Const (id, typ)]
 
 let mk_asset id fds = fun m -> m @ [Asset (id, fds)]
 
-let mk_identifier id typ = (id, Identifier typ)
+let mk_identifier id typ = (id, FIdentifier typ)
 
-let mk_field id typ = (id, Basic typ)
+let mk_field id typ = (id, FBasic typ)
 
-let mk_field_ref id typ = (id, Ref typ)
+let mk_field_ref id typ = (id, FRef typ)
+
+let mk_arg id typ = (id, ABasic typ)
+
+let mk_arg_ref id typ = (id, ARef typ)
 
 let mk_state_machine id states =
   fun m -> m @ [ Machine (id, { empty_machine with mStates = states; })]
@@ -134,6 +153,8 @@ let mk_transition smid trid tr =
       | _ -> e
     )
 
+let mk_transaction id fds = fun m -> m @ [Transaction (id, fds)]
+
 (***********************************************************************************
   test
  ***********************************************************************************)
@@ -148,7 +169,11 @@ let _ =
       mk_cons "total"  Uint >>
       mk_asset "tokenHolder" [
           mk_identifier   "holder"  Address;
-          mk_field        "balance" Uint
+          mk_field        "balance" Uint;
+        ] >>
+      mk_transaction "transfer" [
+          mk_arg "toAddress"  Address;
+          mk_arg "nbTokens"   Uint;
         ]
   in
   print_endline (dump_model erc20_model);
@@ -177,7 +202,7 @@ let _ =
 type uint    = int
 type double
 type date
-type address = FakeAddress
+type address
 type tez
 
 type sender = {
@@ -189,20 +214,21 @@ type assets
 
 (* dynamically generated structs *)
 
+(* identifier is not generated *)
 type tokenHolder = {
-    holder : address;
     balance : uint;
   }
 
 let getTokenHolder : assets -> address -> tokenHolder =
-  fun _ _ -> { holder = FakeAddress; balance = 0; }
+  fun _ _ -> { balance = 0; }
 
 let setTokenHolder : assets -> tokenHolder -> assets = fun a _ -> a
 
 (* actual transation code ****************************************************)
 
-let transfer iAssets iSender iQty =
-  let lTokenholder = getTokenHolder iAssets iSender.address in
-  let lQty = lTokenholder.balance in
-  let lTokenholder = { lTokenholder with balance = lQty + iQty } in
-  setTokenHolder iAssets lTokenholder
+let[@Escrow transfer] transfer iAssets iSender ~toAddress:iTo ~nbTokens:iQty =
+  let lFromholder = getTokenHolder iAssets iSender.address in
+  let lFromholder = { balance = lFromholder.balance - iQty; } in
+  let lToholder   = getTokenHolder iAssets iTo in
+  let lToholder   = { balance = lToholder.balance + iQty; } in
+  setTokenHolder (setTokenHolder iAssets lToholder) lFromholder
